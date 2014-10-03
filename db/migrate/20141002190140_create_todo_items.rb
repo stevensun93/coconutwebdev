92d16a4a6f5a0f9bb1d08a88eeac6666a8bf7b6d
小鼎子 <Falcon@sundingwendemacbook.local>
2014-10-03 12:39:14 -0700

    First submission of Lab 2

---
+++ db/migrate/20141002190140_create_todo_items.rb
@@ -0,0 +1,8 @@
+class CreateTodoItems < ActiveRecord::Migration
+  def change
+    create_table :todo_items do|table|
+      table.string :description
+      table.string :due_date
+    end
+  end
+end
---
+++ models/TodoItems.rb
@@ -0,0 +1,3 @@
+class TodoItem < ActiveRecord::Base
+  
+end
